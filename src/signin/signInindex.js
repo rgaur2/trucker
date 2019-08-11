93dba258a349060a055930baada5e57417ca98d3
rishabh Gaur <rishabhgaur090@gmail.com>
2019-08-11 12:30:31 -0700

    SignIn Feature Complete

--- src/signin/signInindex.js
+++ src/signin/signInindex.js
@@ -1,13 +1,6 @@
 This is java script page
-<<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Application</title>
-    <<link rel="stylesheet" type ="text/css" href="style.css">
-  </head>
-  <body>
-    This is Home page of Trucker appplication
 
-  </body>
-</html>
+(function ()
+{
+  console.log('signin')
+})();
